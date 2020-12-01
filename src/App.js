40bc47a3d0c13e334fc6e1ed22fef20da798c073
mooky1007 <49158801+mooky1007@users.noreply.github.com>
2020-12-02 00:55:40 +0900

    1202

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    name: 'mooky'
+  }
+  render(){
+    const { name } = this.state
+    console.log('render')
+    return (
+      <div>
+      hello {name}!!
+      <button onClick={this.testCustomFunction}>change</button>
+      </div>
+    )
+  }
+  testCustomFunction = () => {
+    this.setState(current => (current.name === "mooky" ? {name: "joon"} : {name: "mooky"}));
+  }
+  componentDidMount(){
+    console.log('did mount')
+  }
+  componentDidUpdate(){
+    console.log('updated')
+  }
 }
+
+// function App(){
+//   return(
+//     <div>
+//       hello3
+//     </div>
+//   )
+// }
 
 export default App;
